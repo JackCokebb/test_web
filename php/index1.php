3cc35cff1e3df5c4ea9eaca7724372c2d113d844
JackCokebb <reo2726@gmail.com>
2022-01-30 21:21:24 +0900

    php test

--- php/index1.php
+++ php/index1.php
@@ -6,7 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>WEB</h1>
+    <h1><a href="index1.php">WEB</a></h1>
     <ol>
         <li><a href = "index1.php?id=HTML">HTML</a></li>
         <li><a href = "index1.php?id=CSS">CSS</a></li>
@@ -14,11 +14,21 @@
     </ol>
     <h2>
         <?php
-        echo $_GET['id'];
+        if(isset($_GET['id'])){
+        echo $_GET['id'];}
+        else{
+           echo "Welcome!";
+        }
         ?>
     </h2>
     <?php
+    if(isset($_GET['id'])){
     echo file_get_contents("data/".$_GET['id']);
+    }
+    else{
+        echo "i am php";
+    }
     ?>
+
 </body>
 </html>
