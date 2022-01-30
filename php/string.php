05476d5ba38866de0945492be146cd9ce3144f04
JackCokebb <reo2726@gmail.com>
2022-01-30 17:29:06 +0900

    php test

--- php/string.php
+++ php/string.php
@@ -7,6 +7,18 @@
     echo '</br>';
     echo "hello world!";
     ?>   
+    <h2>concatenation operator</h2>
+    <?php
+    echo "hello "."world!";
+    ?>
+    <h2>string length function</h2>
+    <?php
+    $str = 'abc def g';
+    echo $str." = length => ".strlen($str);
+    //echo " = length => ";
+    //echo strlen($str);
+    //echo ""
+    ?>
     </body>
 
 </html>
---
+++ php/var.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h2>variable test</h2>
+        <?php
+            $a = 1;
+            echo "1+1 = ";
+            echo $a+1;
+        ?>
+        <h2>random string</h2>
+        <?php
+        $name = "JEE";
+        echo "Lorem ipsum dolor sit amet,".$name." consectetur adipiscing elit. Mauris in vehicula ante, nec fringilla ligula. Mauris ultricies, arcu sit amet feugiat molestie, nulla lorem gravida turpis, a efficitur ligula nulla egestas quam. Curabitur maximus tellus a molestie malesuada. Nam vestibulum mollis risus, vel dictum nulla laoreet id. Phasellus dui quam, finibus quis finibus a, euismod vel nulla. Sed laoreet convallis purus sed sodales. Vivamus ultricies suscipit lorem, et gravida leo vehicula vel. Vestibulum nisl massa, cursus vitae tellus a, auctor scelerisque dui. Etiam faucibus a turpis nec venenatis. Suspendisse potenti.
+
+Curabitur condimentum neque ligula, et tristique massa fringilla id. Aenean commodo commodo nisl, sit amet fringilla velit suscipit nec. Proin rutrum augue a viverra molestie. Duis non leo tristique, iaculis lorem eget, condimentum sem. Sed nec orci magna. Phasellus in sem lacus. Morbi varius interdum nisi, non condimentum tortor condimentum vel. Praesent mattis libero lectus, ac imperdiet erat rhoncus pretium. Cras ultricies odio porta est suscipit vestibulum. Duis commodo, orci a semper commodo, tellus lectus maximus turpis, vitae dapibus ante ante eu est. Curabitur eu molestie turpis. Proin aliquam sem id purus blandit, id vestibulum quam ornare. Donec a auctor justo. Morbi ac vehicula eros, sed elementum ipsum. Pellentesque odio quam, luctus sit amet orci quis, bibendum commodo nisi. Curabitur ac malesuada neque.
+
+In at orci vitae arcu facilisis elementum a ".$name."sit amet nunc. Morbi vulputate mi at libero molestie, non egestas augue consectetur. Etiam vulputate mattis lectus, quis blandit nibh lacinia non. Duis pretium pretium dignissim. Proin ut urna eu ante pulvinar ultrices. Mauris ac pulvinar metus, aliquet consectetur magna. Suspendisse tempor metus quam, eget viverra nisl molestie ornare. Nam accumsan blandit lacus.
+
+Aliquam non nisl turpis. Nam mollis risus ut".$name." risus molestie elementum. Cras vulputate sed dui sed vehicula. Vivamus dignissim a dolor id aliquet. Suspendisse blandit orci vel hendrerit iaculis. Mauris ac est et neque volutpat suscipit ut vitae lectus. Donec in auctor nisi, non finibus odio. Etiam ac dolor hendrerit ex posuere congue. Phasellus libero nibh, molestie nec velit a, ultrices blandit urna. Phasellus id condimentum ligula. Proin rutrum ante vitae leo facilisis, sed sollicitudin felis scelerisque. In vestibulum lorem a varius posuere. Donec ac consectetur dolor.
+
+Nulla ".$name."purus nunc, hendrerit a pulvinar eu, rhoncus id lectus. Proin lacinia tellus ac pharetra vestibulum. Nunc vulputate, augue nec ultrices eleifend, erat lorem bibendum lacus, in vulputate ligula massa egestas nibh. Sed porttitor urna in ligula placerat, id semper felis tempus. Curabitur ultricies finibus massa, vel suscipit ipsum efficitur id. Curabitur felis sem, dictum eget bibendum et, rhoncus et quam. Praesent pulvinar lacus diam, sit amet varius eros hendrerit a. Nunc vel ullamcorper est. Sed vitae nisl sapien. Duis mollis luctus augue. Pellentesque porttitor pellentesque euismod."
+        ?>
+    </body>
+</html>
