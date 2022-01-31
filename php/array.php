e3637d2d8af4124e9996f4d699dd9f4cc89d75b3
JackCokebb <reo2726@gmail.com>
2022-01-31 12:46:42 +0900

    php test

---
+++ php/array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>array</h1>
+    <?php
+    echo "<h2>\$num = array(\"1\",2,3,\"4\",\"5\");</h2>";
+    $num = array("1",2,3,"4","5");
+    $i = 0 ;
+    while($i<count($num)){
+        echo var_dump($num[$i])."<br>";
+        $i++;
+    }
+    ?>
+</body>
+</html>
