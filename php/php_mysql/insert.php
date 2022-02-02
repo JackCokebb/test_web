5e6cbcc9ed2e9c55b798b01ab2852277b3ad1665
JackCokebb <reo2726@gmail.com>
2022-02-02 13:52:44 +0900

    php connect mysql log

--- php/php_mysql/insert.php
+++ php/php_mysql/insert.php
@@ -1,12 +1,16 @@
 <?php
 $conn = mysqli_connect('localhost', 'jee', '1234','php_mysql_db');
-$res = mysqli_query($conn,"
-    INSERT INTO topic
-        (title,description,created)
-        VALUE(
-            'MySQL',
-            'Mysql test 123',
-            NOW()
-        )
-")
+$sql = "
+INSERT INTO topic
+    (title,description,created)
+    VALUE(
+        'MySQL',
+        'Mysql test 123',
+        NOW()
+    )";
+$res = mysqli_query($conn,$sql);
+if(!$res){
+    printf("Error message: %s\n", mysqli_error($conn)); //recommand to save this log in file because of security issue
+}
+
 ?>
